cc2460c8569662126173394185c83c17c4def40f
Daniel Hackney <dhackney@lcs.com>
2020-09-02 14:46:57 -0400

    Minor fixes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes : Routes = [
   {
+    path: 'products',
+    component: ProductListComponent
+  },
+  {
     path: 'product/:id',
     component: ProductPageComponent
   },
